46abf4bd2d7e9d9995dba694ca3ad4599b2c50c3
jijijaganadh <118527638+jijijaganadh@users.noreply.github.com>
2023-06-22 11:51:01 +0530

    user registration with email verification and activation.

--- editor_api/urls.py
+++ editor_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "editor_api"
 urlpatterns = [
-
-        path("addconference/", views.AddConference.as_view(), name="addconference"),
-
+    path("addconference/", views.AddConference.as_view(), name="addconference"),
+    path("registration/", views.Registration, name="registration"),
+    path("activate-user/<uidb64>/<token>", views.activate_user, name="activate"),
 ]
