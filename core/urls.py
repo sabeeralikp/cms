193eabf31f4277a9cb315b755b0a6a98d27cdd21
Sabeerali KP <44801609+sabeeralikp@users.noreply.github.com>
2023-06-30 14:53:08 +0530

    urls updated

--- core/urls.py
+++ core/urls.py
@@ -24,3 +24,7 @@ urlpatterns = [
     path("",include('editor_api.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
